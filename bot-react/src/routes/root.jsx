47496642905797b1bd2e2ed2094d6b2cf2841c89
Rosshoven <Michaeledwardross@gmail.com>
2023-02-19 11:59:39 -0600

    Navbar color change

--- bot-react/src/routes/root.jsx
+++ bot-react/src/routes/root.jsx
@@ -8,7 +8,7 @@ export default function Root() {
         <div>
           <h1>React Router Example</h1>
          
-<nav class="navbar navbar-expand-lg bg-body-">
+<nav class="navbar navbar-expand-lg bg-body-secondary">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">Navbar scroll</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
