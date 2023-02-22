e58c890b544c571056aa6e60309187c48ba5e11a
Rosshoven <Michaeledwardross@gmail.com>
2023-02-22 14:10:00 -0600

    Changed Navbar Scroll to Navbar

--- bot-react/src/routes/root.jsx
+++ bot-react/src/routes/root.jsx
@@ -9,7 +9,7 @@ export default function Root() {
          
 <nav className="navbar navbar-expand-lg bg-body-secondary sticky-top">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar Scroll</a>
+    <a className="navbar-brand" href="/">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
