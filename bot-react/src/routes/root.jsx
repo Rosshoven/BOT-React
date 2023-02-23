d991e10821f6632c1bd03db9f5687d33015b1e6d
Rosshoven <Michaeledwardross@gmail.com>
2023-02-23 10:39:29 -0600

    added .js to bootstrap import

--- bot-react/src/routes/root.jsx
+++ bot-react/src/routes/root.jsx
@@ -1,6 +1,6 @@
 import { Link, Outlet } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export default function Root() {
     return (
